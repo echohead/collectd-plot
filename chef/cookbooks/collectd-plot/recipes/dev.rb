202d654bfbf3487c3cf2a982cc92d45503e2782a
Tim Miller <tim.miller.0@gmail.com>
2012-04-30 11:06:40 -0700

    fix chef directory structure.

---
+++ chef/cookbooks/collectd-plot/recipes/dev.rb
@@ -0,0 +1,23 @@
+# this is a recipe for running collectd-plot inside a vagrant vm.
+# it expects for the root of this repo to be mounted within the vm
+# as /vagrant/.
+
+packages = [
+  'ruby-1.9.1'
+]
+
+packages.each do |p|
+  package p
+    action :install
+  end
+end
+
+# install gems
+script 'Install Bundled Gems' do
+  interpreter 'bash'
+  cwd '/vagrant'
+  user 'root'
+  code <<-EOS
+    bundle isntall --quiet --development'
+  EOS
+end
