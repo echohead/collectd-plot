ae34c49976dea34c74712dcac3980278b011044d
Tim Miller <tim.miller.0@gmail.com>
2012-05-02 14:56:57 -0700

    add more specs to config.

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -17,6 +17,16 @@ describe 'config' do
     CollectdPlot::Config.baz.should == [1, 2]
   end
 
+  it 'should allow properties to be accessed via hash syntax with strings' do
+    CollectdPlot::Config.from_hash :foo => :bar, :baz => [1, 2]
+    CollectdPlot::Config['foo'].should == :bar
+  end
+
+  it 'should allow properties to be accessed via hash syntax with symbols' do
+    CollectdPlot::Config.from_hash :foo => :bar, :baz => [1, 2]
+    CollectdPlot::Config[:foo].should == :bar
+  end
+
   it 'should be able to initialize from a json file' do
     CollectdPlot::Config.from_file("#{File.dirname(__FILE__)}/fixtures/config.json")
     CollectdPlot::Config.arbitrary_key.should == 'foobar'
