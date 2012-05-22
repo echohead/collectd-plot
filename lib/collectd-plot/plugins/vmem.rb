fb36356d97faa90012b7e56defef316d79e692f7
Tim Miller <tim.miller.0@gmail.com>
2012-05-22 10:20:16 -0700

    vmem plugin.

--- lib/collectd-plot/plugins/vmem.rb
+++ lib/collectd-plot/plugins/vmem.rb
@@ -3,19 +3,48 @@
 module CollectdPlot
   module Plugins
     module Vmem
+
+      def self.get_series(host, type)
+        RRDRead.rrd_files(host, 'vmem').select { |f| f =~ Regexp.new('^' + type) }.map { |f| f.gsub(Regexp.new(type + '-'), '') }.map { |f| f.gsub(/.rrd$/, '') }
+      end
+
       def self.massage_graph_opts!(opts)
-        opts[:title] = "users"
-        opts[:ylabel] = 'users'
-        opts[:series] = {
-          'users' => {:rrd => 'users', :value => 'value'}
-        }
+        case opts[:type]
+        when 'vmpage_faults'
+          opts[:title] = 'page faults'
+          opts[:ylabel] = ''
+          opts[:series] = {
+            'minor' => {:rrd => opts[:type], :value => 'minflt', :color => '0000CC'},
+            'major' => {:rrd => opts[:type], :value => 'majflt', :color => 'FF0000'}
+          }
+        when 'vmpage_io'
+          opts[:title] = 'page i/o'
+          opts[:ylabel] = ''
+          opts[:series] = {
+            'memory (in) ' => {:rrd => 'vmpage_io-memory', :value => 'in'},
+            'memory (out)' => {:rrd => 'vmpage_io-memory', :value => 'out'},
+            'swap   (in) ' => {:rrd => 'vmpage_io-swap', :value => 'in'},
+            'swap   (out)' => {:rrd => 'vmpage_io-swap', :value => 'out'}
+          }
+        when 'vmpage_number'
+          opts[:title] = 'pages'
+          opts[:ylabel] = ''
+          opts[:series] = {}
+          ['active_anon', 'active_file', 'anon_pages', 'bounce', 'dirty', 'file_pages', 'free_pages', 'inactive_anon',
+           'inactive_file', 'mapped', 'mlock', 'page_table_pages', 'slab_reclaimable', 'slab_unreclaimable', 'unevictable',
+           'unstable', 'writeback', 'writeback_temp'].each do |s|
+            opts[:series][s] = {:rrd => "vmpage_number-#{s}", :value => 'value'}
+          end
+        else raise "unknown type: #{opts[:type]}"
+        end
+
         opts[:line_width] = 1
-        opts[:graph_type] = :line
-        opts[:rrd_format] = '%.1lf'
+        opts[:graph_type] = :stacked
+        opts[:rrd_format] = '%5.1lf%s'
       end
 
       def self.types()
-        []
+        ['vmpage_faults', 'vmpage_io', 'vmpage_number']
       end
     end
   end
