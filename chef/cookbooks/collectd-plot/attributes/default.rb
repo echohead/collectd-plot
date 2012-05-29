fbcf711d744d44e74267e69e42761bb5238f2613
Tim Miller <tim.miller.0@gmail.com>
2012-05-29 11:08:27 -0700

    remove cookbook from source repo.

--- chef/cookbooks/collectd-plot/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['collectd-plot']['host_groups'] = {
-  'nova' => 'compute-[0-9]+',
-  'localdomain' => 'localdomain$',
-  'bock' => 'bock\d+',
-  'mongo' => 'mongodb'
-}
--- chef/cookbooks/collectd-plot/recipes/dev.rb
+++
@@ -1,9 +0,0 @@
-# this is a recipe for running collectd-plot inside a vagrant vm.
-
-include_recipe 'collectd-plot::common'
-
-script 'Start the service with shotgun' do
-  interpreter 'bash'
-  cwd node[:collectd_plot][:root]
-  code 'sudo nohup shotgun --port 80 --host 10.0.2.15 lib/collectd-plot/config.ru >/dev/null 2>&1 &'
-end
