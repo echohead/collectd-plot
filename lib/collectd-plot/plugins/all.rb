d4b0f8bb13e8f5e343234ac367bee92ece6f2f05
Tim Miller <tim.miller.0@gmail.com>
2012-06-14 16:09:08 -0700

    tweak api.

--- lib/collectd-plot/plugins/all.rb
+++ lib/collectd-plot/plugins/all.rb
@@ -55,11 +55,12 @@ module CollectdPlot
 
     def self.rrds_for(dir_name)
       p = plugin_for dir_name
-      res = []
-      p.types(instance_for(dir_name)).each do |t|
-        opts = { :type => t}
-        p.massage_graph_opts! opts
-        res.concat opts[:series].values.map { |h| { :rrd => h[:rrd], :value => h[:value]} }
+      [].tap do |res|
+        p.types(instance_for(dir_name)).each do |t|
+          opts = { :type => t}
+          p.massage_graph_opts! opts
+          res.concat opts[:series].values.map { |h| { :rrd => h[:rrd], :value => h[:value]} }
+        end
       end
     end
 
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -22,7 +22,7 @@ describe 'the service' do
 
     it 'should list metrics for a host' do
       JSON.parse(get_json('/host/host_a').body).should == 
-{}
+{"cpu"=>{"0"=>[{"rrd"=>"cpu-idle", "value"=>"value"}, {"rrd"=>"cpu-nice", "value"=>"value"}, {"rrd"=>"cpu-user", "value"=>"value"}, {"rrd"=>"cpu-wait", "value"=>"value"}, {"rrd"=>"cpu-system", "value"=>"value"}, {"rrd"=>"cpu-softirq", "value"=>"value"}, {"rrd"=>"cpu-interrupt", "value"=>"value"}, {"rrd"=>"cpu-steal", "value"=>"value"}]}, "df"=>{"root"=>[{"rrd"=>"df_complex-reserved", "value"=>"value"}, {"rrd"=>"df_complex-free", "value"=>"value"}, {"rrd"=>"df_complex-used", "value"=>"value"}]}, "load"=>{""=>[{"rrd"=>"load", "value"=>"shortterm"}, {"rrd"=>"load", "value"=>"midterm"}, {"rrd"=>"load", "value"=>"longterm"}]}, "memory"=>{""=>[{"rrd"=>"memory-free", "value"=>"value"}, {"rrd"=>"memory-buffered", "value"=>"value"}, {"rrd"=>"memory-cached", "value"=>"value"}, {"rrd"=>"memory-used", "value"=>"value"}]}}
     end
 
     it 'should list instances for a metric' do
